6cd44c85933d933aab5138d1600665497f145c84
gabriellebibeau <bibeaugabrielle@gmail.com>
2024-03-25 13:48:22 -0400

    Q3_fig

--- Devoir2_Q1.R
+++ Devoir2_Q1.R
@@ -48,7 +48,7 @@ ggplot() +
   theme_minimal()
 
 
-#Modèle Consommateur-Ressource ----
+#Tests Modèle Consommateur-Ressource ----
 
 f_log <- function(t, R, R_max, r) { #Croissance de Bogustonia proii
   res <- r*R*(1-(R/R_max)) #logistique
@@ -75,11 +75,11 @@ h_lin <- function(t, m, C) { #mortalité d'Aleastonia
   return(res)
 }
 
-Bp_Ap_CR1 <- function(t, Cond_Ini, parms = c(R_max, r, a, c, epsilon, m)){
+Bp_Ap_CR1 <- function(t, Cond_Ini, parms = c(R_max, r, ac, epsilon, m)){
   
   with(as.list(Cond_Ini, parms), {
     
-    #Croissance logistique
+    #Modèle
     dR = f_log(t,R,R_max,r) - g_lin(t, a, c, R, C)
     dC = epsilon*g_lin(t, a, c, R, C) - h_lin(t, m, C)
     
@@ -117,35 +117,96 @@ Bp_Ap_CR3 <- function(t, Cond_Ini, parms = c(R_max, r, a, c, b, epsilon, m)){
   })
 }
 
+Bp_Ap_CR4 <- function(t, Cond_Ini, parms = c(r, ac, epsilon, m)){
+  
+  with(as.list(Cond_Ini, parms), {
+    
+    #Modèle
+    dR = R*r - g_lin(ac, R, C)
+    dC = epsilon*g_lin(ac, R, C) - h_lin(m, C)
+    
+    # Resultat
+    res <- c(dR=dR, dC=dC)
+    return(list(res))
+  })
+}
+
 #Conditions initiales (selon l'énoncé)
 R0 <- 25
 C0 <- 20
 CI <- c(R=R0, C=C0)
 
 #Paramètres
-R_max <- 500 #500
-r <- 3 #3
-a <- 1
-c <- 2.5 #2
-epsilon <- 1
-m <- 0.5 #1
-alpha <- 1
+R_max <- 500  #500
+r <- 3        #3
+a <- 1        #1
+c <- 2        #2
+epsilon <- 1  #1
+m <- 1      #1
+alpha <- 0.1  
 b <- 1
+ac <- 2
 parms1 <- c(R_max=R_max, r=r, a=a, c=c, epsilon=epsilon, m=m)
 parms2 <- c(r=r,alpha=alpha, a=a, c=c, epsilon=epsilon, m=m)
 parms3 <- c(R_max=R_max, r=r, a=a, c=c, b=b, epsilon=epsilon, m=m)
+parms4 <- c(r=r, ac=ac, epsilon=epsilon, m=m)
 
 #solution du système d'équations
-soln <- ode(y=CI, times= seq(1,337), Bp_Ap_CR1, parms1)
+soln <- ode(y=CI, times= seq(1,337), Bp_Ap_CR1, parms1) #je pense que c'est le meilleur
 soln <- ode(y=CI, times= seq(1,337), Bp_Ap_CR2, parms2)
 soln <- ode(y=CI, times= seq(1,337), Bp_Ap_CR3, parms3)
+soln <- ode(y=CI, times= seq(1,337), Bp_Ap_CR4, parms4)
 
 ggplot() +
   geom_line(aes(soln[,'time'], soln[,'R']), color = 'cyan2') +
   geom_line(aes(soln[,'time'], soln[,'C']), color = 'salmon2') +
-  labs(title = "Modèle Consommateur-Ressource", x = "Temps écoulé (1: 2h)", y = "Nombre d'individus") +
+  labs(title = "Modèle Consommateur-Ressource", x = "Temps écoulé (h)", y = "Nombre d'individus") +
   coord_cartesian(xlim = c(0,337))+
   theme_minimal()
+
+#Modèle finale juste in case ----
+
+f_log <- function(R, R_max, r) { #Croissance de Bogustonia proii
+  res <- r*R*(1-(R/R_max)) #logistique
+  return(res)
+}
+
+g_lin <- function(ac, R, C) { #taux de consommation de Bogustonia par Aleastonia
+  res <- ac*R*C #linéaire
+  return(res)
+}
+
+h_lin <- function(m, C) { #mortalité d'Aleastonia
+  res <- m*C #taux constant
+  return(res)
+}
+
+Bp_Ap_CR <- function(t, Cond_Ini, parms = c(R_max, r, ac, epsilon, m)){
+  
+  with(as.list(Cond_Ini, parms), {
+    
+    #Modèle
+    dR = f_log(R,R_max,r) - g_lin(ac, R, C)
+    dC = epsilon*g_lin(ac, R, C) - h_lin(m, C)
+    
+    # Resultat
+    res <- c(dR=dR, dC=dC)
+    return(list(res))
+  })
+}
+
+CI_final <- c(R=25, C=20)
+para_final <- c(R_max=260, r=2.8, ac=1.9, epsilon=1, m=0.5)
+soln_final <- ode(y=CI_final, times= seq(1,337), func= Bp_Ap_CR, parms= para_final)
+
+ggplot() +
+  geom_line(aes(soln_final[,'time'], soln_final[,'R']), color = 'cyan2') +
+  geom_line(aes(soln_final[,'time'], soln_final[,'C']), color = 'salmon2') +
+  geom_line(aes(donnees$X, donnees$bogustonia_proii), color = 'blue2') +
+  geom_line(aes(donnees$X, donnees$aleastonia_predatora), color = 'red2') +
+  labs(title = "Modèle Consommateur-Ressource", x = "Temps écoulé (h)", y = "Nombre d'individus") +
+  theme_minimal()
+
 
 
 #Graphique données expérimentales ----
@@ -162,6 +223,75 @@ df <- data.frame(x = c(x, x), y = c(y1, y2), group = rep(c("Bogustonia proii", "
 
 ggplot(df, aes(x = x, y = y, color = group)) +
   geom_line() +
-  labs(title = "Variation des population de A. predatora et B proii dans un pétri sur 2 semaines.", x = "Temps écoulé (1: 2h)", y = "Nombre d'individus") +
+  labs(title = "Variation des population de A. predatora et B proii dans un pétri sur 2 semaines.", x = "Temps écoulé (h)", y = "Nombre d'individus") +
   theme_minimal()
 
+
+#Montecarlo - corrélation optimale ----
+
+#Conditions initiales (selon l'énoncé)
+R0 <- 25
+C0 <- 20
+CI <- c(R=R0, C=C0)
+
+iterations <- 1
+meilleure_correlation <- 0
+meilleur_Rmax <- 0
+meilleur_r <- 0
+meilleur_ac <- 0
+meilleur_epsilon <- 0
+meilleur_m <- 0
+
+for(iterations in 1:1000){
+  
+  print(iterations)
+  
+  #Parametre
+  R_max <- runif(1,35,500)
+  r <- runif(1,2.5,3.5)
+  ac <- runif(1,1,2)
+  epsilon <- runif(1,0.5,1)
+  m <- runif(1,0.5,1)
+  para <- c(R_max=R_max, r=r, ac=ac, epsilon=epsilon, m=m)
+  
+  #calcul de la solution
+  RC_soln <- ode(y=CI, times= seq(1,337), func= Bp_Ap_CR, parms= para)
+
+  if(length(donnees$bogustonia_proii) == nrow(RC_soln)){
+  
+  #Corrélation
+    Bp <- as.numeric(RC_soln[,'R'])
+    Pearson_Bp <- cor.test(Bp, donnees$bogustonia_proii, method = "pearson")
+    estimate_Bp <- Pearson_Bp$estimate
+    
+    Ap <- as.numeric(RC_soln[,'C'])
+    Pearson_Ap <- cor.test(Ap, donnees$aleastonia_predatora, method = "pearson")
+    estimate_Ap <- Pearson_Ap$estimate
+    
+    estimate <- estimate_Bp + estimate_Ap
+  
+  #enregistrement des paramètres
+    if(estimate > meilleure_correlation){
+        meilleure_correlation <- estimate
+        meilleur_Rmax <- R_max
+        meilleur_r <- r
+        meilleur_ac <- ac
+        meilleur_epsilon <- epsilon
+        meilleur_m <- m
+    } #fin if enregistrement
+        
+  } #fin if taille vecteurs
+  
+  iterations <- iterations + 1
+  
+}#fin boucle for
+
+para_cor <- c(R_max=meilleur_Rmax,r=meilleur_r,ac=meilleur_ac,
+              epsilon=meilleur_epsilon,m=meilleur_m)
+soln_cor <- ode(y=CI, times= seq(1,337), func= Bp_Ap_CR, parms= para_cor)
+
+ggplot() +
+  geom_line(aes(soln_cor[,'time'], soln_cor[,'R']), color = 'cyan2') +
+  geom_line(aes(soln_cor[,'time'], soln_cor[,'C']), color = 'salmon2') +
+  labs(title = "Modèle Consommateur-Ressource", x = "Temps écoulé (h)", y = "Nombre d'individus") +
+  theme_minimal()
--- Devoir2_Q3.R
+++ Devoir2_Q3.R
@@ -1,4 +1,5 @@
 library(deSolve)
+library(ggplot2)
 
 #modèle stochastique ----
 source('Devoir2_Q2.R')
@@ -31,11 +32,21 @@ e <- 1
 para <- c(c=c,e=e, N=N)
 
 #Simulation stochastique
-mod_sto_100 <- metapopGill(c,e,N,N0,3) #Extinction, que faire?
-plot(mod_sto_100)
+mat_sto_100 <- matrix(NA, nrow = 61, ncol = 4)
+mat_sto_100[,1] <- seq(0,60)
+for (i in 1:3) {
+  sto_100 <- metapopGill(c,e,N,N0,60)
+  mat_sto_100[,i+1] <- sto_100[,'P']
+}
+
+ggplot()+
+  geom_line(aes(mat_sto_100[,1], mat_sto_100[,2]), color = 'red') +
+  geom_line(aes(mat_sto_100[,1], mat_sto_100[,3]), color = 'green') +
+  geom_line(aes(mat_sto_100[,1], mat_sto_100[,4]), color = 'orange')
+
 
 #Simulation méchanistique
-mod_mec_100 <- ode(y=c(P=P0), times=seq(1,3), metapopMech, para) #monte à 0,5 très rapidement (t=2) et s'y stabilise
+mod_mec_100 <- ode(y=c(P=P0), times=seq(1,100), metapopMech, para) #monte à 0,5 très rapidement (t=2) et s'y stabilise
 plot(mod_mec_100[,'time'], mod_mec_100[,'P'])
 #pas probl?matique, voir cours 11 diapo 23
 
@@ -52,8 +63,13 @@ e <- 1
 para <- c(c=c,e=e, N=N)
 
 #Simulation stochastique
-mod_sto_1000 <- metapopGill(c,e,N,N0,3) 
-plot(mod_sto_1000)
+mat_sto_1000 <- matrix(NA, nrow = 61, ncol = 4)
+mat_sto_1000[,1] <- seq(0,60)
+for (i in 1:3) {
+  sto_1000 <- metapopGill(c,e,N,N0,60)
+  mat_sto_1000[,i+1] <- sto_1000[,'P']
+}
+
 
 #Simulation méchanistique
 mod_mec_1000 <- ode(y=P0, times=seq(1,3), metapopMech, para)
@@ -72,8 +88,38 @@ e <- 1
 para <- c(c=c,e=e, N=N)
 
 #Simulation stochastique
-mod_sto_10000 <- metapopGill(c,e,N,N0,3) 
-plot(mod_sto_10000)
+mat_sto_10000 <- matrix(NA, nrow = 61, ncol = 4)
+mat_sto_10000[,1] <- seq(0,60)
+for (i in 1:3) {
+  sto_10000 <- metapopGill(c,e,N,N0,60)
+  mat_sto_10000[,i+1] <- sto_10000[,'P']
+}
+
+
+#Simulation méchanistique
+mod_mec_10000 <- ode(y=P0, times=seq(1,3), metapopMech, para)
+plot(mod_mec_10000[,'time'], mod_mec_10000[,'1'])
+
+
+#Simulation N=100000 ----
+#Conditions initiales
+N0 <- 1
+N <- 100000
+P0 <- N0/N
+
+#Paramètres
+c <- 2
+e <- 1
+para <- c(c=c,e=e, N=N)
+
+#Simulation stochastique
+mat_sto_100000 <- matrix(NA, nrow = 61, ncol = 4)
+mat_sto_100000[,1] <- seq(0,60)
+for (i in 1:3) {
+  sto_100000 <- metapopGill(c,e,N,N0,60)
+  mat_sto_100000[,i+1] <- sto_100000[,'P']
+}
+
 
 #Simulation méchanistique
 mod_mec_10000 <- ode(y=P0, times=seq(1,3), metapopMech, para)
